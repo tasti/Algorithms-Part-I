9765d5a3a86cb2ff7578646b1210963a9afa35fd
Tasti Zakarie <tasti.zakarie@gmail.com>
2014-06-28 15:36:47 -0400

    Add programming assg

--- job-interview-questions/stacks-and-queues/q4.py
+++ job-interview-questions/stacks-and-queues/q4.py
@@ -1,1 +1,14 @@
 from StacksAndQueues import Node
+
+def isCyclic(llist):
+    slow = llist
+    fast = llist
+
+    while slow and fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow and fast and (slow == fast):
+            return slow.item
+
+    return False
